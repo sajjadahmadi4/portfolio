c93bcc7568a924796812f429e46a35036ad1731a
Sajjad Ahmadi <49483687+sajjadahmadi4@users.noreply.github.com>
2023-08-01 19:57:25 +0330

    added responsive breakpoints to the project section
    tweaked main.js and added some responsive break points.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,23 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      860: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      }
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    
   });
